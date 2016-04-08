9e4f411ae32cb4e6439d1579f32b4d27c6aec38f
Alan Duncan <duncan.alan@mac.com>
2016-04-08 05:39:28 -0400

    Added setup
    Added a script that automates the prerequisites

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os, subprocess, sys
+subprocess.call(['python', 'virtualenv.py', 'flask'])
+if sys.platform == 'win32':
+    bin = 'Scripts'
+else:
+    bin = 'bin'
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-login'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy==0.7.9'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask-sqlalchemy'])
+subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'sqlalchemy-migrate'])
